fd27199c955de7e0668680e2b444f37a131a452d
nmeier01 <nmeier01@student.ubc.ca>
2026-03-17 15:51:48 -0700

    Updated stats in R

--- scripts/visualization.R
+++ scripts/visualization.R
@@ -10,3 +10,21 @@ stomataTidy %>%
   geom_boxplot()+
   scale_fill_manual(values = c("high" = "white", "low" = "darkgrey"))+
   labs(x = "Species", y="Stomatal Count", fill = "Light Condition")
+
+
+
+blackberryData <- stomataTidy %>% 
+  filter(species =="blackberry")
+
+blackberryStats <- blackberryData%>%
+  filter(!is.na(stomata_avg))
+
+ivyData <- stomataTidy %>% 
+  filter(species =="ivy")
+
+ivyStats <- ivyData %>%
+  filter(!is.na(stomata_avg))
+
+t.test(as.numeric(stomata_avg) ~ light_condition, data = ivyStats)
+
+t.test(as.numeric(stomata_avg) ~ light_condition, data = blackberryStats)
